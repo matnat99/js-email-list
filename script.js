ae0745640fa9491bd1b5f08966cace68b1f9fc8a
mattianatale <mattianatale07@gmail.com>
2025-01-08 17:01:49 +0100

    feat: add bonus

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-// DOM Elements
-const emailListElm = document.getElementById('email-list')
-
-// DOM Events
-// On Page Load
-for(let i = 0; i < 10; i++){
+// Functions
+const load = () =>{
+  emailListElm.innerHTML = ''
+  
+  for(let i = 0; i < 10; i++){
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then((res) =>{
     const email = res.data.response
     emailListElm.innerHTML += `<li>${email}</li>`
   })
 }
+}
+
+// DOM Elements
+const emailListElm = document.getElementById('email-list')
+const btnReloadElm = document.getElementById('btn-reload')
+
+// DOM Events
+// On Page Load
+load()
+
+// On Click
+btnReloadElm.addEventListener("click", load)
