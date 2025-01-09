26801dc311ecb391ea6029a1ac81f85b236fceb9
mattianatale <mattianatale07@gmail.com>
2025-01-09 17:39:45 +0100

    feat: add loading all elements together

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // Functions
 const load = () =>{
   emailListElm.innerHTML = ''
+  const emailList = []
   
   for(let i = 0; i < 10; i++){
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then((res) =>{
     const email = res.data.response
-    emailListElm.innerHTML += `<li>${email}</li>`
+    emailList.push(`<li>${email}</li>`)
+    if(emailList.length === 10){
+      emailListElm.innerHTML = emailList.join('')
+    }
   })
 }
 }
